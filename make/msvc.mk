4f1ce188de3048b89c44e2ca77c6c28ceb7e0a97
Jay Berkenbilt <ejb@ql.org>
2009-10-11 01:02:08 +0000

    working msvc configuration
    git-svn-id: svn+q:///qpdf/trunk@778 71b93d88-0707-0410-a8cf-f5a4172ac649

--- make/msvc.mk
+++ make/msvc.mk
@@ -13,6 +13,12 @@ define binname
 $(1).exe
 endef
 
+# --- Local Changes ---
+
+# Filter out -g
+CFLAGS := $(filter-out -g,$(CFLAGS))
+CXXFLAGS := $(filter-out -g,$(CXXFLAGS))
+
 # --- Required rule definitions ---
 
 #                       1   2
@@ -40,26 +46,27 @@ define makeslib
 	lib /nologo /OUT:$(2) $(1)
 endef
 
-#                       1    2       3       4        5
-# Usage: $(call makelib,objs,library,current,revision,age)
+#                       1    2       3       4    5       6        7
+# Usage: $(call makelib,objs,library,ldflags,libs,current,revision,age)
 define makelib
-	cl /nologo /Zi /Gy /EHsc /MD /LD /Fe$(basename $(2))$(3).dll $(1) \
+	cl /nologo /Zi /Gy /EHsc /MD /LD /Fe$(basename $(2))$(5).dll $(1) \
 		/link /incremental:no \
-		$(foreach L,$(subst -L,,$(LDFLAGS)),/LIBPATH:$(L)) \
-		$(foreach L,$(subst -l,,$(LIBS)),$(L).lib)
-	if [ -f $(basename $(2))$(3).dll.manifest ]; then \
-		mt.exe -nologo -manifest $(basename $(2))$(3).dll.manifest \
-			-outputresource:$(basename $(2))$(3).dll\;2; \
+		$(foreach L,$(subst -L,,$(3)),/LIBPATH:$(L)) \
+		$(foreach L,$(subst -l,,$(4)),$(L).lib)
+	if [ -f $(basename $(2))$(5).dll.manifest ]; then \
+		mt.exe -nologo -manifest $(basename $(2))$(5).dll.manifest \
+			-outputresource:$(basename $(2))$(5).dll\;2; \
 	fi
+	mv $(basename $(2))$(5).lib $(2)
 endef
 
-#                       1    2
-# Usage: $(call makebin,objs,binary)
+#                       1    2      3       4
+# Usage: $(call makebin,objs,binary,ldflags,libs)
 define makebin
 	cl /nologo /Zi /Gy /EHsc /MD $(1) \
 		/link /incremental:no /OUT:$(2) \
-		$(foreach L,$(subst -L,,$(LDFLAGS)),/LIBPATH:$(L)) \
-		$(foreach L,$(subst -l,,$(LIBS)),$(L).lib)
+		$(foreach L,$(subst -L,,$(3)),/LIBPATH:$(L)) \
+		$(foreach L,$(subst -l,,$(4)),$(L).lib)
 	if [ -f $(2).manifest ]; then \
 		mt.exe -nologo -manifest $(2).manifest \
 			-outputresource:$(2)\;2; \
