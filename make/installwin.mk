82c4a8ff0f3b2cba0e446aad59596eed467415ba
Jay Berkenbilt <ejb@ql.org>
2009-10-22 15:57:41 +0000

    push install down a level
    git-svn-id: svn+q:///qpdf/trunk@861 71b93d88-0707-0410-a8cf-f5a4172ac649

--- make/installwin.mk
+++ make/installwin.mk
@@ -1,18 +1,20 @@
+DEST=$(INSTALL_DIR)/$(PACKAGE_TARNAME)-$(PACKAGE_VERSION)
 installwin: all
 	$(RM) -r $(INSTALL_DIR)
 	mkdir $(INSTALL_DIR)
-	mkdir $(INSTALL_DIR)/bin
-	mkdir $(INSTALL_DIR)/lib
-	mkdir $(INSTALL_DIR)/include
-	mkdir $(INSTALL_DIR)/include/qpdf
-	mkdir $(INSTALL_DIR)/doc
-	cp libqpdf/$(OUTPUT_DIR)/$(STATIC_LIB_NAME) $(INSTALL_DIR)/lib
-	cp libqpdf/$(OUTPUT_DIR)/qpdf*.dll $(INSTALL_DIR)/bin
-	cp qpdf/$(OUTPUT_DIR)/qpdf.exe $(INSTALL_DIR)/bin
-	cp zlib-flate/$(OUTPUT_DIR)/zlib-flate.exe $(INSTALL_DIR)/bin
-	cp qpdf/fix-qdf $(INSTALL_DIR)/bin
-	cp include/qpdf/*.h $(INSTALL_DIR)/include/qpdf
-	cp include/qpdf/*.hh $(INSTALL_DIR)/include/qpdf
-	cp doc/stylesheet.css $(INSTALL_DIR)/doc
-	cp doc/qpdf-manual.html $(INSTALL_DIR)/doc
-	cp doc/qpdf-manual.pdf $(INSTALL_DIR)/doc
+	mkdir $(DEST)
+	mkdir $(DEST)/bin
+	mkdir $(DEST)/lib
+	mkdir $(DEST)/include
+	mkdir $(DEST)/include/qpdf
+	mkdir $(DEST)/doc
+	cp libqpdf/$(OUTPUT_DIR)/$(STATIC_LIB_NAME) $(DEST)/lib
+	cp libqpdf/$(OUTPUT_DIR)/qpdf*.dll $(DEST)/bin
+	cp qpdf/$(OUTPUT_DIR)/qpdf.exe $(DEST)/bin
+	cp zlib-flate/$(OUTPUT_DIR)/zlib-flate.exe $(DEST)/bin
+	cp qpdf/fix-qdf $(DEST)/bin
+	cp include/qpdf/*.h $(DEST)/include/qpdf
+	cp include/qpdf/*.hh $(DEST)/include/qpdf
+	cp doc/stylesheet.css $(DEST)/doc
+	cp doc/qpdf-manual.html $(DEST)/doc
+	cp doc/qpdf-manual.pdf $(DEST)/doc
