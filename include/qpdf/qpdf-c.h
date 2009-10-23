75ea1971fa275613380dacef5dee0fc0bb9ccc45
Jay Berkenbilt <ejb@ql.org>
2009-10-23 14:59:50 +0000

    include copyright notice in public headers
    git-svn-id: svn+q:///qpdf/trunk@883 71b93d88-0707-0410-a8cf-f5a4172ac649

--- include/qpdf/qpdf-c.h
+++ include/qpdf/qpdf-c.h
@@ -1,3 +1,11 @@
+/* Copyright (c) 2005-2009 Jay Berkenbilt
+ *
+ * This file is part of qpdf.  This software may be distributed under
+ * the terms of version 2 of the Artistic License which may be found
+ * in the source distribution.  It is provided "as is" without express
+ * or implied warranty.
+ */
+
 #ifndef __QPDF_C_H__
 #define __QPDF_C_H__
 
