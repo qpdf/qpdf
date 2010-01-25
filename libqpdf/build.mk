e5fbae0a376b8d91d9c6f3636a1a2f39d2b28dce
Jay Berkenbilt <ejb@ql.org>
2010-01-25 02:57:58 +0000

    typo
    git-svn-id: svn+q:///qpdf/trunk@940 71b93d88-0707-0410-a8cf-f5a4172ac649

--- libqpdf/build.mk
+++ libqpdf/build.mk
@@ -72,4 +72,4 @@ $(OBJS_libqpdf): libqpdf/$(OUTPUT_DIR)/%.$(LOBJ): libqpdf/%.cc
 # * Otherwise, increment REVISION
 
 $(TARGETS_libqpdf): $(OBJS_libqpdf)
-	$(call makelib,$(OBJS_libqpdf),$@,$(LDFLAGS),$(LIBS),3,0,1)
+	$(call makelib,$(OBJS_libqpdf),$@,$(LDFLAGS),$(LIBS),3,1,0)
