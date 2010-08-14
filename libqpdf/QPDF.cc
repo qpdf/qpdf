f7a18c42a0142f36d8cc575fcd10f0e4638ba084
Jay Berkenbilt <ejb@ql.org>
2010-08-14 15:55:17 +0000

    remove stray +
    git-svn-id: svn+q:///qpdf/trunk@1012 71b93d88-0707-0410-a8cf-f5a4172ac649

--- libqpdf/QPDF.cc
+++ libqpdf/QPDF.cc
@@ -2059,6 +2059,6 @@ QPDF::getAllPagesInternal(QPDFObjectHandle cur_pages,
 	throw QPDFExc(qpdf_e_damaged_pdf, this->file.getName(),
 		      this->last_object_description,
 		      this->file.getLastOffset(),
-		      + ": invalid Type in page tree");
+		      ": invalid Type in page tree");
     }
 }
