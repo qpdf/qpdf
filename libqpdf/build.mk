8e9fe21316ec33a190ce8c22dfdecccb494808e5
Jay Berkenbilt <ejb@ql.org>
2011-12-28 17:19:40 -0500

    Update for 2.3.1

--- libqpdf/build.mk
+++ libqpdf/build.mk
@@ -72,4 +72,4 @@ $(OBJS_libqpdf): libqpdf/$(OUTPUT_DIR)/%.$(LOBJ): libqpdf/%.cc
 # * Otherwise, increment REVISION
 
 $(TARGETS_libqpdf): $(OBJS_libqpdf)
-	$(call makelib,$(OBJS_libqpdf),$@,$(LDFLAGS),$(LIBS),7,0,4)
+	$(call makelib,$(OBJS_libqpdf),$@,$(LDFLAGS),$(LIBS),7,1,4)
