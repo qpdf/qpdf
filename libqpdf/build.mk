7e4a079674b91d273d15cdbaa10b0fce65916368
Jay Berkenbilt <ejb@ql.org>
2012-09-06 15:31:35 -0400

    Update libtool data for API changes

--- libqpdf/build.mk
+++ libqpdf/build.mk
@@ -80,4 +80,4 @@ $(OBJS_libqpdf): libqpdf/$(OUTPUT_DIR)/%.$(LOBJ): libqpdf/%.cc
 # * Otherwise, increment REVISION
 
 $(TARGETS_libqpdf): $(OBJS_libqpdf)
-	$(call makelib,$(OBJS_libqpdf),$@,$(LDFLAGS),$(LIBS),8,2,0)
+	$(call makelib,$(OBJS_libqpdf),$@,$(LDFLAGS),$(LIBS),9,0,1)
