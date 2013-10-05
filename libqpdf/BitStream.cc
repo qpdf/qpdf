eb1b1264b46f02550201e3e5856ff575fa47a0f7
Jay Berkenbilt <ejb@ql.org>
2013-10-05 05:51:54 -0400

    Security: fix potential multiplication overflow
    Better sanity check inputs to bit stream reader

--- libqpdf/BitStream.cc
+++ libqpdf/BitStream.cc
@@ -16,6 +16,10 @@ BitStream::reset()
 {
     p = start;
     bit_offset = 7;
+    if (static_cast<unsigned int>(nbytes) > static_cast<unsigned int>(-1) / 8)
+    {
+	throw std::runtime_error("array too large for bitstream");
+    }
     bits_available = 8 * nbytes;
 }
 
