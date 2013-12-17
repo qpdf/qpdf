0b6127558d94a8b5ea1cdaca2c58e5b5a0cd1472
Jay Berkenbilt <ejb@ql.org>
2013-12-17 15:26:07 -0500

    Prepare 5.1.0 release

--- libqpdf/QPDF.cc
+++ libqpdf/QPDF.cc
@@ -20,7 +20,7 @@
 #include <qpdf/QPDF_Null.hh>
 #include <qpdf/QPDF_Dictionary.hh>
 
-std::string QPDF::qpdf_version = "5.0.1";
+std::string QPDF::qpdf_version = "5.1.0";
 
 static char const* EMPTY_PDF =
     "%PDF-1.3\n"
