247d70efee7e55436f119e7fe5a067f8f6dbf03a
Jay Berkenbilt <ejb@ql.org>
2014-01-14 15:45:35 -0500

    Prepare 5.1.1 release

--- libqpdf/build.mk
+++ libqpdf/build.mk
@@ -96,4 +96,4 @@ $(COBJS_libqpdf): libqpdf/$(OUTPUT_DIR)/%.$(LOBJ): libqpdf/%.c
 # * Otherwise, increment REVISION
 
 $(TARGETS_libqpdf): $(OBJS_libqpdf)
-	$(call makelib,$(OBJS_libqpdf),$@,$(LDFLAGS),$(LIBS),14,0,1)
+	$(call makelib,$(OBJS_libqpdf),$@,$(LDFLAGS),$(LIBS),14,1,1)
