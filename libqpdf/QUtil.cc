3b2a3cdd77957e90ab8c07094666d8bdd2e43993
Jay Berkenbilt <ejb@ql.org>
2018-02-04 13:52:44 -0500

    Fix setLineBuf for bsd (fixes #177)
    Use 0 instead of NULL in a cast.

--- libqpdf/QUtil.cc
+++ libqpdf/QUtil.cc
@@ -354,7 +354,7 @@ void
 QUtil::setLineBuf(FILE* f)
 {
 #ifndef _WIN32
-    setvbuf(f, reinterpret_cast<char *>(NULL), _IOLBF, 0);
+    setvbuf(f, reinterpret_cast<char *>(0), _IOLBF, 0);
 #endif
 }
 
