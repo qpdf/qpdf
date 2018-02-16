1fdd86a04924e4ab9543133b74df3322cffbd358
Jay Berkenbilt <ejb@ql.org>
2018-02-15 20:25:49 -0500

    Move Pl_QPDFTokenizer to public interface

--- include/qpdf/Pl_QPDFTokenizer.hh
+++ include/qpdf/Pl_QPDFTokenizer.hh
@@ -1,3 +1,24 @@
+// Copyright (c) 2005-2018 Jay Berkenbilt
+//
+// This file is part of qpdf.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Versions of qpdf prior to version 7 were released under the terms
+// of version 2.0 of the Artistic License. At your option, you may
+// continue to consider qpdf to be licensed under those terms. Please
+// see the manual for additional information.
+
 #ifndef __PL_QPDFTOKENIZER_HH__
 #define __PL_QPDFTOKENIZER_HH__
 
@@ -11,6 +32,11 @@
 // in turn to a QPDFObjectHandle::TokenFilter object. All bytes of
 // incoming content will be included in exactly one token and passed
 // downstream.
+
+// This is a very low-level interface for working with token filters.
+// Most code will want to use QPDFObjectHandle::filterPageContents or
+// QPDFObjectHandle::addTokenFilter. See QPDFObjectHandle.hh for
+// details.
 
 class Pl_QPDFTokenizer: public Pipeline
 {
