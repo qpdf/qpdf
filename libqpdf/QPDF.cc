6ee761fc860fe6b22d8c46f250d09b8cfdd1986f
Jay Berkenbilt <ejb@ql.org>
2018-08-18 10:56:19 -0400

    Prepare 8.2.1 release

--- libqpdf/QPDF.cc
+++ libqpdf/QPDF.cc
@@ -19,7 +19,7 @@
 #include <qpdf/QPDF_Null.hh>
 #include <qpdf/QPDF_Dictionary.hh>
 
-std::string QPDF::qpdf_version = "8.2.0";
+std::string QPDF::qpdf_version = "8.2.1";
 
 static char const* EMPTY_PDF =
     "%PDF-1.3\n"
