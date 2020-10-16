1bcd8c1649e65e2be82a8839c857355eb4d3196b
Jay Berkenbilt <ejb@ql.org>
2020-10-16 11:17:42 -0400

    Rename azure-pipelines to build-scripts

--- build-scripts/build-windows.bat
+++ build-scripts/build-windows.bat
@@ -7,10 +7,10 @@ if %2 == msvc (
        call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
     )
     choco install zip
-    bash ./azure-pipelines/build-windows %1 %2
+    bash ./build-scripts/build-windows %1 %2
 ) else (
     @rem The vs2017-win2016 pool has an ancient 64-bit-only mingw.
     @rem Install msys2 so we can get current gcc toolchains.
     choco install msys2
-    C:\tools\msys64\usr\bin\env.exe MSYSTEM=MINGW64 /bin/bash -l %CD%/azure-pipelines/build-windows %1 %2
+    C:\tools\msys64\usr\bin\env.exe MSYSTEM=MINGW64 /bin/bash -l %CD%/build-scripts/build-windows %1 %2
 )
