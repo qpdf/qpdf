37ad3908a6904b190ceed1b0e52a2057267904f1
Jay Berkenbilt <ejb@ql.org>
2022-03-12 10:09:54 -0500

    fixup! Convert build-windows to use cmake

--- build-scripts/build-windows.bat
+++ build-scripts/build-windows.bat
@@ -1,14 +1,15 @@
 @echo on
 @rem Usage: build-windows {32|64} {msvc|mingw}
 setlocal ENABLEDELAYEDEXPANSION
-set MSYS=C:\msys64
 if %2 == msvc (
     if %1 == 64 (
        call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
     ) else (
        call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
     )
-    !MSYS!\usr\bin\bash.exe -l %CD%/build-scripts/build-windows %1 %2
+    choco install zip
+    bash ./build-scripts/build-windows %1 %2
 ) else (
+    set MSYS=C:\msys64
     !MSYS!\usr\bin\env.exe MSYSTEM=MINGW64 /bin/bash -l %CD%/build-scripts/build-windows %1 %2
 )
