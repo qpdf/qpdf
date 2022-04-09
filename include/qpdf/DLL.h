ec219100668357e46cc8640386e2332379956b7c
Jay Berkenbilt <ejb@ql.org>
2022-04-09 14:46:43 -0400

    Bump version to 11.0.0, indicating ABI-breaking changes

--- include/qpdf/DLL.h
+++ include/qpdf/DLL.h
@@ -24,10 +24,10 @@
 #define QPDF_DLL_HH
 
 /* The first version of qpdf to include the version constants is 10.6.0. */
-#define QPDF_MAJOR_VERSION 10
-#define QPDF_MINOR_VERSION 6
-#define QPDF_PATCH_VERSION 3
-#define QPDF_VERSION "10.6.3"
+#define QPDF_MAJOR_VERSION 11
+#define QPDF_MINOR_VERSION 0
+#define QPDF_PATCH_VERSION 0
+#define QPDF_VERSION "11.0.0"
 
 #if (defined _WIN32 || defined __CYGWIN__) && defined(DLL_EXPORT)
 # define QPDF_DLL __declspec(dllexport)
