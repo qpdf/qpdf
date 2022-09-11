416d9668ee97c54a692d9280364746ce8aeeed43
m-holger <m-holger@kubitscheck.org>
2022-09-11 18:13:34 +0100

    Add default move constructor/assignment to QPDFObjectHandle

--- include/qpdf/QPDFObjectHandle.hh
+++ include/qpdf/QPDFObjectHandle.hh
@@ -330,7 +330,11 @@ class QPDFObjectHandle
     QPDF_DLL
     QPDFObjectHandle(QPDFObjectHandle const&) = default;
     QPDF_DLL
+    QPDFObjectHandle(QPDFObjectHandle&&) = default;
+    QPDF_DLL
     QPDFObjectHandle& operator=(QPDFObjectHandle const&) = default;
+    QPDF_DLL
+    QPDFObjectHandle& operator=(QPDFObjectHandle&&) = default;
     QPDF_DLL
     inline bool isInitialized() const;
 
