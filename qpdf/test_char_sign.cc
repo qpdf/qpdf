77111086eb38f6075fd2e8e4da74acc32719be02
Jay Berkenbilt <ejb@ql.org>
2022-09-23 17:43:01 -0400

    Add code to CI to verify signed/unsigned char
    Make sure that our attempt to test both signed and unsigned char is
    actually right.

---
+++ qpdf/test_char_sign.cc
@@ -0,0 +1,11 @@
+#include <cstdio>
+int main()
+{
+    char ch = '\xf7';
+    if (ch < 0) {
+        printf("char is signed\n");
+    } else {
+        printf("char is unsigned\n");
+    }
+    return 0;
+}
