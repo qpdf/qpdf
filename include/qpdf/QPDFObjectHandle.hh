4c515d2ab869421c027ad4adb3dc9f6a41ab2d0f
m-holger <m-holger@kubitscheck.org>
2022-11-14 22:06:04 +0000

    Refactor QPDFObjectHandle::shallowCopy

--- include/qpdf/QPDFObjectHandle.hh
+++ include/qpdf/QPDFObjectHandle.hh
@@ -1647,8 +1647,6 @@ class QPDFObjectHandle
     void objectWarning(std::string const& warning);
     void assertType(char const* type_name, bool istype);
     bool dereference();
-    void copyObject1(std::set<QPDFObjGen>& visited);
-    void shallowCopyInternal1(QPDFObjectHandle& oh);
     void copyObject(
         std::set<QPDFObjGen>& visited,
         bool cross_indirect,
