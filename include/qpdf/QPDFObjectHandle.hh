ef02aa2f7bf39cadc8e59cd847cc61d5b071e488
m-holger <m-holger@kubitscheck.org>
2022-11-20 15:15:04 +0000

    Remove redundant parameter first_level_only from QPDFObjectHandle::shallowCopyInternal2 and copyObject2

--- include/qpdf/QPDFObjectHandle.hh
+++ include/qpdf/QPDFObjectHandle.hh
@@ -1647,8 +1647,8 @@ class QPDFObjectHandle
     void objectWarning(std::string const& warning);
     void assertType(char const* type_name, bool istype);
     bool dereference();
-    void copyObject1(std::set<QPDFObjGen>& visited, bool first_level_only);
-    void shallowCopyInternal1(QPDFObjectHandle& oh, bool first_level_only);
+    void copyObject1(std::set<QPDFObjGen>& visited);
+    void shallowCopyInternal1(QPDFObjectHandle& oh);
     void copyObject(
         std::set<QPDFObjGen>& visited,
         bool cross_indirect,
