c78f44798a0b07e7a585c85c7b8bcc1907542cd4
m-holger <m-holger@kubitscheck.org>
2023-03-21 16:28:07 +0000

    Add new _qpdf_data constructor taking a std::unique_ptr<QPDF>
    Also, move _qpdf_data and to new header filer _qpdf_error to new header
    file qpdf-c_impl.hh

--- libqpdf/qpdf-c.cc
+++ libqpdf/qpdf-c.cc
@@ -12,6 +12,7 @@
 #include <qpdf/QPDFWriter.hh>
 #include <qpdf/QTC.hh>
 #include <qpdf/QUtil.hh>
+#include <qpdf/qpdf-c_impl.hh>
 #include <qpdf/qpdflogger-c_impl.hh>
 
 #include <cstring>
@@ -19,42 +20,6 @@
 #include <list>
 #include <stdexcept>
 #include <string>
-
-struct _qpdf_error
-{
-    std::shared_ptr<QPDFExc> exc;
-};
-
-struct _qpdf_data
-{
-    _qpdf_data() = default;
-    ~_qpdf_data() = default;
-
-    std::shared_ptr<QPDF> qpdf;
-    std::shared_ptr<QPDFWriter> qpdf_writer;
-
-    std::shared_ptr<QPDFExc> error;
-    _qpdf_error tmp_error;
-    std::list<QPDFExc> warnings;
-    std::string tmp_string;
-
-    // Parameters for functions we call
-    char const* filename{nullptr}; // or description
-    char const* buffer{nullptr};
-    unsigned long long size{0};
-    char const* password{nullptr};
-    bool write_memory{false};
-    std::shared_ptr<Buffer> output_buffer;
-
-    // QPDFObjectHandle support
-    bool silence_errors{false};
-    bool oh_error_occurred{false};
-    std::map<qpdf_oh, std::shared_ptr<QPDFObjectHandle>> oh_cache;
-    qpdf_oh next_oh{0};
-    std::set<std::string> cur_iter_dict_keys;
-    std::set<std::string>::const_iterator dict_iter;
-    std::string cur_dict_key;
-};
 
 // must set qpdf->filename and qpdf->password
 static void
