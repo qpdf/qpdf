qpdf/qpdf

commit deb1c330869b2785fac8ec3916a67a0ff6916a3d

Replace strchr in QPDF_Name::normalizeName

libqpdf/QPDF_Name.cc
@@ -37,7 +37,10 @@ QPDF_Name::normalizeName(std::string const& name)
             // QPDFTokenizer embeds a null character to encode an
             // invalid #.
             result += "#";
-        } else if (strchr("#()<>[]{}/%", ch) || (ch < 33) || (ch > 126)) {
+        } else if (
+            ch < 33 || ch == '/' || ch == '(' || ch == ')' || ch == '{' ||
+            ch == '}' || ch == '<' || ch == '>' || ch == '[' || ch == ']' ||
+            ch == '%' || ch > 126) {
             result += QUtil::hex_encode_char(ch);
         } else {
             result += ch;

commit 3f2ebf334b2d42b84243da589fe41df0e28cc2e8

Fix bug in QPDF_Name::normalizeName introduced in #891
'#' was omitted from the list of chars that need to be escaped.

libqpdf/QPDF_Name.cc
@@ -38,9 +38,9 @@ QPDF_Name::normalizeName(std::string const& name)
             // invalid #.
             result += "#";
         } else if (
-            ch < 33 || ch == '/' || ch == '(' || ch == ')' || ch == '{' ||
-            ch == '}' || ch == '<' || ch == '>' || ch == '[' || ch == ']' ||
-            ch == '%' || ch > 126) {
+            ch < 33 || ch == '#' || ch == '/' || ch == '(' || ch == ')' ||
+            ch == '{' || ch == '}' || ch == '<' || ch == '>' || ch == '[' ||
+            ch == ']' || ch == '%' || ch > 126) {
             result += QUtil::hex_encode_char(ch);
         } else {
             result += ch;

qpdf/qtest/qpdf/pound-in-name.pdf
@@ -81,6 +81,7 @@ trailer <<
     /a#b [ 4 /##00## /Som#ething ]
     /Som#ething (else)
   >>
+  /#20#23#2f#28#29#7b#7d#3c#3e#5b#5d#25#21#7e#7f 42
 >>
 
 startxref

qpdf/qtest/qpdf/pound-in-name.qdf
@@ -92,6 +92,7 @@ xref
 0000000629 00000 n 
 0000000774 00000 n 
 trailer <<
+  /#20#23#2f#28#29#7b#7d#3c#3e#5b#5d#25!~#7f 42
   /Other#20Tests <<
     /## 1
     /#20# 2