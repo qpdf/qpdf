1db8a8b26f9daee323e57e3b28f61fdf39be3ae1
Jay Berkenbilt <ejb@ql.org>
2023-09-02 15:23:41 -0400

    Bump version to 11.6

--- include/qpdf/DLL.h
+++ include/qpdf/DLL.h
@@ -25,9 +25,9 @@
 
 /* The first version of qpdf to include the version constants is 10.6.0. */
 #define QPDF_MAJOR_VERSION 11
-#define QPDF_MINOR_VERSION 5
+#define QPDF_MINOR_VERSION 6
 #define QPDF_PATCH_VERSION 0
-#define QPDF_VERSION "11.5.0"
+#define QPDF_VERSION "11.6.0"
 
 /*
  * This file defines symbols that control the which functions,
