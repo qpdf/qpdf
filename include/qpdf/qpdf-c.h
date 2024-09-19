af383e717b99e79c61c898ab3454bfef05829c39
Chao Li(VISION) <lchao@microsoft.com>
2024-09-19 12:18:26 +0000

    Add C API qpdf_free_buffer to release memory allocated by stream data functions

--- include/qpdf/qpdf-c.h
+++ include/qpdf/qpdf-c.h
@@ -927,6 +927,14 @@ extern "C" {
     QPDF_ERROR_CODE qpdf_oh_get_page_content_data(
         qpdf_data qpdf, qpdf_oh page_oh, unsigned char** bufp, size_t* len);
 
+    /* Call free to release the buffer allocated with malloc. This function can be used to free 
+     * buffers that were dynamically allocated by qpdf functions such as qpdf_oh_get_stream_data
+     * or qpdf_oh_get_page_content_data. The caller is responsible for calling qpdf_free_buffer
+     * to manage memory properly and avoid memory leaks.
+     */
+    QPDF_DLL
+    void qpdf_free_buffer(unsigned char** bufp);
+
     /* The data pointed to by bufp will be copied by the library. It does not need to remain valid
      * after the call returns.
      */
