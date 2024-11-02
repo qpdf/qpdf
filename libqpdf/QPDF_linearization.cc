2e3f54e616bd00ac2f20df8532dd3cc8e6757f3d
m-holger <m.holger@qpdf.org>
2024-11-02 15:01:00 +0000

    Refactor QPDF::isLinearized

--- libqpdf/QPDF_linearization.cc
+++ libqpdf/QPDF_linearization.cc
@@ -96,10 +96,9 @@ QPDF::isLinearized()
 
     // The PDF spec says the linearization dictionary must be completely contained within the first
     // 1024 bytes of the file. Add a byte for a null terminator.
-    auto buffer =  m->file->read(1024, 0);
-    int lindict_obj = -1;
+    auto buffer = m->file->read(1024, 0);
     size_t pos = 0;
-    while (lindict_obj == -1) {
+    while (true) {
         // Find a digit or end of buffer
         pos = buffer.find_first_of("0123456789"sv, pos);
         if (pos == std::string::npos) {
@@ -109,46 +108,41 @@ QPDF::isLinearized()
         // next iteration.
         m->file->seek(toO(pos), SEEK_SET);
 
-        QPDFTokenizer::Token t1 = readToken(*m->file);
-        if (t1.isInteger() && readToken(*m->file).isInteger() &&
-            readToken(*m->file).isWord("obj")) {
-            lindict_obj = toI(QUtil::string_to_ll(t1.getValue().c_str()));
+        QPDFTokenizer::Token t1 = readToken(*m->file, 20);
+        if (!(t1.isInteger() && readToken(*m->file, 6).isInteger() &&
+              readToken(*m->file, 4).isWord("obj"))) {
+            pos = buffer.find_first_not_of("0123456789"sv, pos);
+            if (pos == std::string::npos) {
+                return false;
+            }
+            continue;
         }
-        pos = buffer.find_first_not_of("0123456789"sv, pos);
-        if (pos == std::string::npos) {
+
+        auto candidate = m->objects.get(toI(QUtil::string_to_ll(t1.getValue().data())), 0);
+        if (!candidate.isDictionary()) {
             return false;
         }
-    }
 
-    if (lindict_obj <= 0) {
-        return false;
-    }
-
-    auto candidate = m->objects.get(lindict_obj, 0);
-    if (!candidate.isDictionary()) {
-        return false;
-    }
-
-    QPDFObjectHandle linkey = candidate.getKey("/Linearized");
-    if (!(linkey.isNumber() && (toI(floor(linkey.getNumericValue())) == 1))) {
-        return false;
-    }
-
-    QPDFObjectHandle L = candidate.getKey("/L");
-    if (L.isInteger()) {
-        qpdf_offset_t Li = L.getIntValue();
-        m->file->seek(0, SEEK_END);
-        if (Li != m->file->tell()) {
-            QTC::TC("qpdf", "QPDF /L mismatch");
+        QPDFObjectHandle linkey = candidate.getKey("/Linearized");
+        if (!(linkey.isNumber() && toI(floor(linkey.getNumericValue())) == 1)) {
             return false;
-        } else {
-            m->linp.file_size = Li;
         }
+
+        QPDFObjectHandle L = candidate.getKey("/L");
+        if (L.isInteger()) {
+            qpdf_offset_t Li = L.getIntValue();
+            m->file->seek(0, SEEK_END);
+            if (Li != m->file->tell()) {
+                QTC::TC("qpdf", "QPDF /L mismatch");
+                return false;
+            } else {
+                m->linp.file_size = Li;
+            }
+        }
+
+        m->lindict = candidate;
+        return true;
     }
-
-    m->lindict = candidate;
-
-    return true;
 }
 
 void
@@ -158,8 +152,7 @@ QPDF::readLinearizationData()
     // that prevent loading.
 
     if (!isLinearized()) {
-        throw std::logic_error("called readLinearizationData for file"
-                               " that is not linearized");
+        throw std::logic_error("called readLinearizationData for file that is not linearized");
     }
 
     // /L is read and stored in linp by isLinearized()
@@ -1621,7 +1614,7 @@ QPDF::calculateHOutline(QPDFWriter::NewObjTable const& new_obj, QPDFWriter::ObjT
 
 template <class T, class int_type>
 static void
-write_vector_int(BitWriter& w, int nitems, std::vector<T>& vec, int bits, int_type T::*field)
+write_vector_int(BitWriter& w, int nitems, std::vector<T>& vec, int bits, int_type T::* field)
 {
     // nitems times, write bits bits from the given field of the ith vector to the given bit writer.
 
@@ -1639,9 +1632,9 @@ write_vector_vector(
     BitWriter& w,
     int nitems1,
     std::vector<T>& vec1,
-    int T::*nitems2,
+    int T::* nitems2,
     int bits,
-    std::vector<int> T::*vec2)
+    std::vector<int> T::* vec2)
 {
     // nitems1 times, write nitems2 (from the ith element of vec1) items from the vec2 vector field
     // of the ith item of vec1.
