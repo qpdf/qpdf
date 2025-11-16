fd644c639b232792c5fd82d5860f2a8c59e3d6aa
m-holger <m.holger@qpdf.org>
2025-11-16 11:41:25 +0000

    Add `inspection_mode` for optional restricted PDF inspection
    Introduce `qpdf::global::inspection_mode` for analyzing and repairing damaged PDFs with a restricted feature set. Update relevant code to enforce restrictions and add supporting test cases.

--- include/qpdf/Constants.h
+++ include/qpdf/Constants.h
@@ -274,11 +274,12 @@ enum qpdf_result_e {
  */
 enum qpdf_param_e {
     /* global options */
+    qpdf_p_inspection_mode = 0x10000,
     qpdf_p_default_limits = 0x10100,
     /* global limits */
 
     /* object / parser limits */
-    qpdf_p_objects_max_nesting = 0x011000,
+    qpdf_p_objects_max_nesting = 0x11000,
     qpdf_p_objects_max_errors,
     qpdf_p_objects_max_container_size,
     qpdf_p_objects_max_container_size_damaged,
