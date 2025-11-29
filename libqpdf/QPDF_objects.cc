a750b8b9d07fd8935bf54191c0e87cd7679bfb9d
m-holger <m.holger@qpdf.org>
2025-11-29 11:50:28 +0000

    Move `QPDFXRefEntry` implementation from separate file to `QPDF_objects.cc` and update `CMakeLists.txt` accordingly.

--- libqpdf/QPDF_objects.cc
+++ libqpdf/QPDF_objects.cc
@@ -26,6 +26,43 @@ using namespace std::literals;
 
 using Objects = QPDF::Doc::Objects;
 
+QPDFXRefEntry::QPDFXRefEntry() = default;
+
+QPDFXRefEntry::QPDFXRefEntry(int type, qpdf_offset_t field1, int field2) :
+    type(type),
+    field1(field1),
+    field2(field2)
+{
+    util::assertion(type == 1 || type == 2, "invalid xref type " + std::to_string(type));
+}
+
+int
+QPDFXRefEntry::getType() const
+{
+    return type;
+}
+
+qpdf_offset_t
+QPDFXRefEntry::getOffset() const
+{
+    util::assertion(type == 1, "getOffset called for xref entry of type != 1");
+    return this->field1;
+}
+
+int
+QPDFXRefEntry::getObjStreamNumber() const
+{
+    util::assertion(type == 2, "getObjStreamNumber called for xref entry of type != 2");
+    return QIntC::to_int(field1);
+}
+
+int
+QPDFXRefEntry::getObjStreamIndex() const
+{
+    util::assertion(type == 2, "getObjStreamIndex called for xref entry of type != 2");
+    return field2;
+}
+
 namespace
 {
     class InvalidInputSource: public InputSource
