167604f9610e4a67b7156d425ade80c57e2d58b5
m-holger <m.holger@qpdf.org>
2026-05-07 00:30:36 +0100

    Refactor fuzzers to use global::options::fuzz_mode

--- fuzz/dct_fuzzer.cc
+++ fuzz/dct_fuzzer.cc
@@ -1,57 +1,38 @@
 #include <qpdf/Pl_DCT.hh>
 #include <qpdf/Pl_Discard.hh>
-#include <cstdlib>
+#include <qpdf/global.hh>
+
 #include <iostream>
 #include <stdexcept>
 
 class FuzzHelper
 {
   public:
-    FuzzHelper(unsigned char const* data, size_t size);
-    void run();
+    FuzzHelper(unsigned char const* data, size_t size) :
+        data(data),
+        size(size)
+    {
+    }
+
+    void
+    run()
+    {
+        qpdf::global::options::fuzz_mode(true);
+
+        Pl_Discard discard;
+        Pl_DCT p("decode", &discard);
+        try {
+            p.write(data, size);
+            p.finish();
+        } catch (std::runtime_error const& e) {
+            std::cerr << "runtime_error: " << e.what() << '\n';
+        }
+    }
 
   private:
-    void doChecks();
-
     unsigned char const* data;
     size_t size;
 };
-
-FuzzHelper::FuzzHelper(unsigned char const* data, size_t size) :
-    data(data),
-    size(size)
-{
-}
-
-void
-FuzzHelper::doChecks()
-{
-    // Limit the memory used to decompress JPEG files during fuzzing. Excessive memory use during
-    // fuzzing is due to corrupt JPEG data which sometimes cannot be detected before
-    // jpeg_start_decompress is called. During normal use of qpdf very large JPEGs can occasionally
-    // occur legitimately and therefore must be allowed during normal operations.
-    Pl_DCT::setMemoryLimit(200'000'000);
-    Pl_DCT::setScanLimit(50);
-
-    // Do not decompress corrupt data. This may cause extended runtime within jpeglib without
-    // exercising additional code paths in qpdf.
-    Pl_DCT::setThrowOnCorruptData(true);
-
-    Pl_Discard discard;
-    Pl_DCT p("decode", &discard);
-    p.write(const_cast<unsigned char*>(data), size);
-    p.finish();
-}
-
-void
-FuzzHelper::run()
-{
-    try {
-        doChecks();
-    } catch (std::runtime_error const& e) {
-        std::cerr << "runtime_error: " << e.what() << '\n';
-    }
-}
 
 extern "C" int
 LLVMFuzzerTestOneInput(unsigned char const* data, size_t size)
