a08a553fc2b73b0494fab0562c47d3dd21c8e14e
Nikhil Dabas <nikhil@nikhildabas.com>
2026-09-04 21:06:07 +0100

    Add vcpkg support for Windows ARM64

--- build-scripts/run-vcpkg.bat
+++ build-scripts/run-vcpkg.bat
@@ -1,13 +1,23 @@
 @echo on
-@rem Usage: run-vcpkg.bat {x64-windows-static|x64-mingw-static|x86-windows-static|x86-mingw-static}
+@rem Usage: run-vcpkg.bat {x64-windows-static|x64-mingw-static|x86-windows-static|x86-mingw-static|arm64-windows-static}
 setlocal ENABLEDELAYEDEXPANSION
 set VCVARS_SCRIPT=
 set VALID_TRIPLET=
+set VC_COMPONENT=Microsoft.VisualStudio.Component.VC.Tools.x86.x64
 if "%1" == "x64-windows-static" (
     set VCVARS_SCRIPT=vcvars64.bat
     set VALID_TRIPLET=1
 ) else if "%1" == "x86-windows-static" (
     set VCVARS_SCRIPT=vcvars32.bat
+    set VALID_TRIPLET=1
+) else if "%1" == "arm64-windows-static" (
+    set VC_COMPONENT=Microsoft.VisualStudio.Component.VC.Tools.ARM64
+    rem Use the cross compiler unless we are already running on ARM64.
+    if /i "%PROCESSOR_ARCHITECTURE%" == "ARM64" (
+        set VCVARS_SCRIPT=vcvarsarm64.bat
+    ) else (
+        set VCVARS_SCRIPT=vcvarsamd64_arm64.bat
+    )
     set VALID_TRIPLET=1
 ) else if "%1" == "x64-mingw-static" (
     set VALID_TRIPLET=1
@@ -15,7 +25,7 @@ if "%1" == "x64-windows-static" (
     set VALID_TRIPLET=1
 )
 if not defined VALID_TRIPLET (
-    echo Usage: run-vcpkg.bat {x64-windows-static^|x64-mingw-static^|x86-windows-static^|x86-mingw-static}
+    echo Usage: run-vcpkg.bat {x64-windows-static^|x64-mingw-static^|x86-windows-static^|x86-mingw-static^|arm64-windows-static}
     exit /b 2
 )
 if defined VCVARS_SCRIPT (
@@ -27,7 +37,7 @@ if defined VCVARS_SCRIPT (
     set VCVARS=
     if exist "!VSWHERE!" (
         rem Find the latest Visual Studio with C++ build tools installed.
-        for /f "usebackq delims=" %%I in (`"!VSWHERE!" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do set VSINSTALL=%%I
+        for /f "usebackq delims=" %%I in (`"!VSWHERE!" -latest -products * -requires !VC_COMPONENT! -property installationPath`) do set VSINSTALL=%%I
         if defined VSINSTALL (
             set VCVARS=!VSINSTALL!\VC\Auxiliary\Build\!VCVARS_SCRIPT!
         )
